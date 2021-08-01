05af00812af73c8b45f209a657cfb7a739ddf2c6
nobitanha <44314450+nobitanha@users.noreply.github.com>
2021-08-01 17:12:58 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/UserProfile";
+import { useState } from "react";
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const OnClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <button onClick={OnClickFetchUser}>データー取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user}></UserCard>
+          ))}
+        </>
+      )}
     </div>
   );
 }
